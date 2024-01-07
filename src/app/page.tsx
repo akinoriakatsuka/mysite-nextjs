70f4bd2dd5ca55f9bdd749fe82324241715849f5
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-01-07 17:56:53 +0900

    app routerを使って記事一覧と記事ページを作成する仕組みを作成
    参考：https://musclecoding.com/nextjs-app-router-markdown-blog/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
-export default function Home() {
-  return <div className="my-8">コンテンツ</div>;
+import Link from "next/link";
+
+export default async function Home() {
+
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            ようこそ
+          </h2>
+          <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16">
+            ここは、私のポートフォリオサイトです。
+            ブログは
+            <Link className="text-blue-700" href="/blog">
+              こちら
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
