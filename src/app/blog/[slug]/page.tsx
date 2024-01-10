77f698bf521d4d21e395336e66c6d04df85e01c8
akinoriakatsuka <akinoriakatsuka@gmail.com>
2024-01-11 00:00:24 +0900

    細かいスタイルを変更

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -75,7 +75,7 @@ export default async function BlogPost({ params }: { params: Props }) {
 
   return (
     <div className="px-6 py-0 lg:px-8">
-      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700 pb-4">
+      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700 pb-6 lg:pb-8">
         <div className="px-6 py-6">
           <h1 className="mt-0 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             {title}
